a012ea45e839579f983c9a1639e946e013a5446c
Charles Zhang <progzhangchao@gmail.com>
2014-04-05 10:19:51 +0800

    init dev

--- BestCommander.pro
+++ BestCommander.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    fileinfo.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    fileinfo.h
 
 FORMS    += mainwindow.ui
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "fileinfo.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    FileInfo *p = new FileInfo;
+    ui->leftView->setModel(p);
 }
 
 MainWindow::~MainWindow()
